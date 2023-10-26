5e17ee56e06f2e8270188b7a03899c90a4333d0a
Gustavolskw <gustavolschmidt13@gmail.com>
2023-10-26 15:30:31 -0300

    Desenvolvimento Aula Alura Programaçao Orientada de Obejtos

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@
 import java.util.Scanner;
 
 import static BasePath.DataBase.*;
-import static ReadPath.ReadMethods.*;
 
 public class Main {
 	public static void main(String[] args) {
-		int option = 0;
-		boolean numValido = true;
+		int option;
+		double saldo = 2500;
+		double novoSaldo;
+		double valTransf;
+		int cont=0;
+		int contMaster=0;
+
 		Scanner escolha = new Scanner(System.in);
+		Scanner transf = new Scanner(System.in);
 
 		System.out.println(separador() + "\n");
 		System.out.println(dataBase());
-		System.out.println(saldo());
+		System.out.println(saldo);
 		System.out.println("\n" + separador() + "\n");
 
 		do {
@@ -22,6 +27,26 @@ public class Main {
 				System.out.println("*** Valor inválido! *** \n" + showOptions());
 				option = escolha.nextInt();
 			}
+			if(option==1 && contMaster==0){
+				System.out.println("Seu Saldo é de "+ saldo+"\n");
+			} else if (option==1 && contMaster>=1) {
+				System.out.println("Seu Saldo é de "+ saldo+" Após "+cont+" Transações\n");
+			} else if (option==2) {
+				System.out.println("Digite o Valor que voce deseja transferir: ");
+				valTransf = transf.nextDouble();
+				novoSaldo=saldo-valTransf;
+				saldo=novoSaldo;
+				cont++;
+			}else if (option==3) {
+				System.out.println("Digite o Valor que voce deseja Receber: ");
+				valTransf = transf.nextDouble();
+				novoSaldo=saldo+valTransf;
+				saldo=novoSaldo;
+				cont++;
+			}
+
+
+			contMaster++;
 		}while (option != 4) ;
 
 
